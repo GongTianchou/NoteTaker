f3526e8abe026b42a6330d1e704da6dc0992cb83
Tianchou Gong <tianchougong@gmail.com>
2022-06-30 07:46:15 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+app.listen(PORT, () => 
+console.log(`App listening on http://localhost:${PORT}🚀`))
